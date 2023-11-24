82981385748bfedf59c92af87b85d747d0470815
Jay-Welsh <37424369+Jay-Welsh@users.noreply.github.com>
2023-11-24 15:53:16 +0100

    Update App button v10.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai';
 import DallELogo from './assets/DallE Logo.png';
 
 const configuration = new Configuration({
@@ -8,17 +8,29 @@ const configuration = new Configuration({
 const apiClient = new OpenAIApi(configuration);
 
 const App = () => {
-  // ... other state definitions
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+  const [buttonType, setButtonType] = useState("");
 
-  const predefinedHashtags = "#BusinessInsights #LinkedInTips"; // Define your hashtags here
+  const handleButtonClick = (type, predefinedPrompt) => {
+    setPrompt(predefinedPrompt);
+    setButtonType(type);
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
+    let finalPrompt = prompt;
+    if (buttonType === "humorous") {
+      finalPrompt += " #Funny #Comedy #Entertainment";
+    } else if (buttonType === "serious") {
+      finalPrompt += " #Business #Professional #Insights";
+    }
+
     try {
       const completions = await apiClient.createCompletion({
         model: "text-davinci-003",
-        prompt: prompt,
+        prompt: finalPrompt,
         max_tokens: 880,
         temperature: 0.7,
         top_p: 1,
@@ -26,45 +38,41 @@ const App = () => {
         presence_penalty: 0,
       });
 
-      const responseWithHashtags = completions.data.choices[0].text + "\n\n" + predefinedHashtags;
-      setResponse(responseWithHashtags); // Append hashtags to the response
+      setResponse(completions.data.choices[0].text);
     } catch (error) {
       console.log(error);
     }
   };
 
-  // ... rest of your existing handleSubmit function
-
   return (
     <div className="flex flex-col items-center justify-center px-4 sm:p-0">
       <img src={DallELogo} alt="DallE Logo" style={{ borderRadius: '50%', transform: 'scale(0.6)' }} />
       <div>
-        <button 
-          onClick={() => handleButtonClick('humorous')} 
-          style={{ backgroundColor: activeButton === 'humorous' ? 'green' : 'initial' }}
-        >
-          Humorous
-        </button>
-        <button 
-          onClick={() => handleButtonClick('serious')} 
-          style={{ backgroundColor: activeButton === 'serious' ? 'green' : 'initial' }}
-        >
-          Serious
-        </button>
+        <button onClick={() => handleButtonClick("humorous", "Make a humorous LinkedIn Post about:")}>Humorous</button>
+        <button onClick={() => handleButtonClick("serious", "Create a serious LinkedIn Post discussing:")}>Serious</button>
       </div>
       <form onSubmit={handleSubmit} className="w-full max-w-lg">
-        {/* ... rest of your form */}
-        <input
-          type="text"
-          placeholder="Enter your topic"
-          value={userInput}
-          onChange={handleUserInputChange}
-        />
-        {/* ... rest of your form */}
+        <div className="flex items-center border-b-2 border-indigo-600 py-2">
+          <input
+            className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            placeholder="Enter a topic for LinkedIn Post"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+          <button
+            className="flex-shrink-0 bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-sm border-4 text-white py-1 px-2 rounded"
+            type="submit"
+          >
+            Generate
+          </button>
+        </div>
       </form>
       {response && (
         <div className="w-full max-w-lg mt-4">
-          {/* ... display the response */}
+          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <p className="text-gray-700 text-base">{response}</p>
+          </div>
         </div>
       )}
     </div>
