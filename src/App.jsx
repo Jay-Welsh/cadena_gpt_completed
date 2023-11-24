58e8a8a4541d1cd038d96748b86db6d4fea1b0a5
Jay-Welsh <37424369+Jay-Welsh@users.noreply.github.com>
2023-11-24 10:59:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
 
   return (
     <div className="flex flex-col items-center justify-center px-4 sm:p-0">
-      <img src={DallELogo} alt="DallE Logo" className="h-20 mb-10 mt-40" />
+      <img src={DallELogo} alt="DallE Logo" className="h-20 mb-15 mt-60" />
       <form onSubmit={handleSubmit} className="w-full max-w-lg">
         <div className="flex items-center border-b-2 border-indigo-600 py-2">
           <input
