9561350b598c5496544c9cabd68494fd7a766ffa
Jay-Welsh <37424369+Jay-Welsh@users.noreply.github.com>
2023-11-24 13:11:26 +0100

    Update App buttons v6.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,21 @@ const App = () => {
   const [userInput, setUserInput] = useState("");
   const [response, setResponse] = useState("");
 
-  const handleButtonClick = (tone) => {
-    const fullPrompt = `${tone}: ${userInput}`;
+  const handleButtonClick = (style) => {
+    let hashtags = style === "Humorous" 
+      ? "#funny #humor #business #comedy"
+      : "#serious #business #professional #leadership";
+    let fullPrompt = `${style} LinkedIn Post: ${userInput} ${hashtags}`;
     setPrompt(fullPrompt);
-    handleSubmit(); // Call the submit function directly
   };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
     try {
       const completions = await apiClient.createCompletion({
         model: "text-davinci-003",
-        prompt: userInput,
+        prompt: prompt,
         max_tokens: 880,
         temperature: 0.7,
         top_p: 1,
@@ -36,37 +40,29 @@ const App = () => {
 
   return (
     <div className="flex flex-col items-center justify-center px-4 sm:p-0">
-      <img src={DallELogo} alt="DallE Logo" style={{ borderRadius: '50%', transform: 'scale(0.5)' }} />
-      <div>
-        <button 
-          onClick={() => handleButtonClick("Humorous Style")}
-          style={{ backgroundColor: 'red', color: 'white', fontWeight: 'bold', fontSize: '1.5em', margin: '10px' }}
-        >
-          Humorous
-        </button>
-        <button 
-          onClick={() => handleButtonClick("Serious Style")}
-          style={{ backgroundColor: 'blue', color: 'white', fontWeight: 'bold', fontSize: '1.5em', margin: '10px' }}
-        >
-          Serious
-        </button>
+      <img src={DallELogo} alt="DallE Logo" style={{ borderRadius: '50%', transform: 'scale(0.6)' }} />
+      <p style={{ color: 'white', margin: '10px' }}>Select either of these for a result:</p>
+      <div style={{ marginBottom: '10px' }}>
+        <button onClick={() => handleButtonClick("Humorous")} style={{ backgroundColor: 'red', color: 'white', borderRadius: '20px', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', marginRight: '5px', padding: '10px 20px' }}>Humorous</button>
+        <button onClick={() => handleButtonClick("Serious")} style={{ backgroundColor: 'blue', color: 'white', borderRadius: '20px', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', padding: '10px 20px' }}>Serious</button>
       </div>
-      <div style={{ margin: '10px 0', width: '100%', textAlign: 'center' }}>
-        <input
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-          placeholder="Enter your topic"
-          className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 leading-tight focus:outline-none"
-          style={{ maxWidth: '300px', margin: '0 auto', display: 'inline-block' }} 
-        />
-        <button
-          onClick={handleSubmit}
-          className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
-          style={{ marginLeft: '10px' }}
-        >
-          Generate
-        </button>
-      </div>
+      <form onSubmit={handleSubmit} className="w-full max-w-lg">
+        <div className="flex items-center border-b-2 border-indigo-600 py-2">
+          <input
+            className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            placeholder="Enter your topic here"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          <button
+            className="flex-shrink-0 bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-sm border-4 text-white py-1 px-2 rounded"
+            type="submit"
+          >
+            Generate
+          </button>
+        </div>
+      </form>
       {response && (
         <div className="w-full max-w-lg mt-4">
           <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
