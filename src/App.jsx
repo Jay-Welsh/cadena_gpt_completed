aaea78329c12dc0272108d4c61fe34c700e28d5d
Jay-Welsh <37424369+Jay-Welsh@users.noreply.github.com>
2023-11-24 16:05:41 +0100

    Update App button v11.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,55 +8,60 @@ const configuration = new Configuration({
 const apiClient = new OpenAIApi(configuration);
 
 const App = () => {
-  const [prompt, setPrompt] = useState("");
-  const [response, setResponse] = useState("");
-  const [buttonType, setButtonType] = useState("");
+  const [prompt, setPrompt] = useState('');
+  const [response, setResponse] = useState('');
+  const [selectedButton, setSelectedButton] = useState('');
 
-  const handleButtonClick = (type, predefinedPrompt) => {
+  const handleButtonClick = (buttonType, predefinedPrompt) => {
     setPrompt(predefinedPrompt);
-    setButtonType(type);
+    setSelectedButton(buttonType);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    let finalPrompt = prompt;
-    if (buttonType === "humorous") {
-      finalPrompt += " #Funny #Comedy #Entertainment";
-    } else if (buttonType === "serious") {
-      finalPrompt += " #Business #Professional #Insights";
-    }
+    // Example hashtags
+    const hashtags = selectedButton === 'humorous'
+      ? '#Funny #Humor #Laugh'
+      : '#Serious #Expert #Advice';
 
     try {
       const completions = await apiClient.createCompletion({
-        model: "text-davinci-003",
-        prompt: finalPrompt,
+        model: 'text-davinci-003',
+        prompt: `${prompt} ${hashtags}`,
         max_tokens: 880,
-        temperature: 0.7,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
+        // ... other configurations
       });
-
       setResponse(completions.data.choices[0].text);
     } catch (error) {
       console.log(error);
     }
   };
 
+  // Render Method
   return (
     <div className="flex flex-col items-center justify-center px-4 sm:p-0">
       <img src={DallELogo} alt="DallE Logo" style={{ borderRadius: '50%', transform: 'scale(0.6)' }} />
       <div>
-        <button onClick={() => handleButtonClick("humorous", "Make a humorous LinkedIn Post about:")}>Humorous</button>
-        <button onClick={() => handleButtonClick("serious", "Create a serious LinkedIn Post discussing:")}>Serious</button>
+        <button 
+          className={`button ${selectedButton === 'humorous' ? 'green' : ''}`} 
+          onClick={() => handleButtonClick('humorous', 'Make a humorous LinkedIn Post about:')}
+        >
+          Humorous
+        </button>
+        <button 
+          className={`button ${selectedButton === 'serious' ? 'green' : ''}`} 
+          onClick={() => handleButtonClick('serious', 'Give a serious expert tip about:')}
+        >
+          Serious
+        </button>
       </div>
       <form onSubmit={handleSubmit} className="w-full max-w-lg">
         <div className="flex items-center border-b-2 border-indigo-600 py-2">
           <input
             className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
-            placeholder="Enter a topic for LinkedIn Post"
+            placeholder="Enter a topic for Linkedin Post"
             value={prompt}
             onChange={(e) => setPrompt(e.target.value)}
           />
