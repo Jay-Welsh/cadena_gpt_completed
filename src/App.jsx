90b6e3f8910966f09a580f56226cab407d0b7c78
Jay-Welsh <37424369+Jay-Welsh@users.noreply.github.com>
2023-11-24 11:04:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
 
   return (
     <div className="flex flex-col items-center justify-center px-4 sm:p-0">
-      <img src={DallELogo} alt="Descriptive Alt Text" style={{ borderRadius: '50%' }} />
+      <img src={DallELogo} alt="Descriptive Alt Text" style={{ borderRadius: '30%' }} />
       <form onSubmit={handleSubmit} className="w-full max-w-lg">
         <div className="flex items-center border-b-2 border-indigo-600 py-2">
           <input
